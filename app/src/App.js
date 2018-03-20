55a19b59f15af2d48a88e384155acba3644fd1ca
VooDooS <thevoodoos@gmail.com>
2018-03-20 16:15:49 +0100

    Progress on popups handling

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import GoldenLayout from 'golden-layout';
-import CreateReactClass from 'create-react-class';
 import logo from './logo.svg';
 import './App.css';
+
+// Import Brace and the AceEditor Component
+//import brace from 'brace';
+import AceEditor from 'react-ace';
+
+// Import a Mode (language)
+import 'brace/mode/ocaml';
+
+// Import a Theme (okadia, github, xcode etc)
+import 'brace/theme/monokai';
+
+import 'brace/ext/language_tools';
+import 'brace/ext/searchbox';
 
 import 'golden-layout/dist/goldenlayout.min.js'
 import 'golden-layout/src/css/goldenlayout-base.css'
@@ -17,30 +29,72 @@ var myLayout = new GoldenLayout({
         type: 'row',
         content:[{
             type:'react-component',
-            component: 'testComponent',
+            component: 'aceComponent',
             props: { label: 'A' }
         },{
             type: 'column',
             content:[{
                 type:'react-component',
-                component: 'testComponent',
+                component: 'aceComponent',
                 props: { label: 'B' }
             },{
                 type:'react-component',
-                component: 'testComponent',
+                component: 'aceComponent',
                 props: { label: 'C' }
             }]
         }]
     }]
 });
 
-var TestComponent = CreateReactClass({
-    render: function() {
-        return (<h1>{this.props.label}</h1>)
-    }
-});
 
-myLayout.registerComponent( 'testComponent', TestComponent );
+class AceComponent extends React.Component {
+    componentWillMount() {
+	this.props.glEventHub.on( 'text_changed', this.setValue, this );
+	this.props.glContainer.on( 'resize', function(){
+	    window.dispatchEvent(new Event('resize'));
+	});
+    }
+
+    componentWillUnmount() {
+	this.props.glEventHub.off( 'text_changed', this.setValue, this );
+    }
+    
+    valChanged(newValue) {
+	this.props.glEventHub.emit('text_changed', newValue);
+    }
+    
+    setValue(newValue) {
+	this.setState({ value: newValue });
+    }
+    
+    render() {
+	var val = "tata";
+	if( this.state ) {
+	    val = this.state.value;
+	}
+	
+	return (
+                <AceEditor
+            mode="ocaml"
+            theme="monokai"
+            onChange={(n) => this.valChanged(n)}
+            name={this.props.label}
+	    width="100%"
+	    height="100%"
+	    value={ val }
+	    setOptions={{
+		enableBasicAutocompletion: true,
+		enableLiveAutocompletion: true,
+		enableSnippets: false,
+		showLineNumbers: true,
+		tabSize: 4,
+	    }}
+                />
+        );
+    }
+}
+
+myLayout.registerComponent( 'aceComponent', AceComponent );
 
 myLayout.init();
 
@@ -61,3 +115,4 @@ class App extends Component {
 }
 
 export default App;
+
