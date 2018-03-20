7d31648918b65bd18ade156e772e96080c2040a4
VooDooS <thevoodoos@gmail.com>
2018-03-20 13:39:43 +0100

    Init

---
+++ app/src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import GoldenLayout from 'golden-layout';
+import CreateReactClass from 'create-react-class';
+import logo from './logo.svg';
+import './App.css';
+//import ReactDOM from 'react-dom'
+
+//window.React = React
+//window.ReactDOM = ReactDOM
+var myLayout = new GoldenLayout({
+    content: [{
+        type: 'row',
+        content:[{
+            type:'react-component',
+            component: 'testComponent',
+            props: { label: 'A' }
+        },{
+            type: 'column',
+            content:[{
+                type:'react-component',
+                component: 'testComponent',
+                props: { label: 'B' }
+            },{
+                type:'react-component',
+                component: 'testComponent',
+                props: { label: 'C' }
+            }]
+        }]
+    }]
+});
+
+var TestComponent = CreateReactClass({
+    render: function() {
+        return (<h1>{this.props.label}</h1>)
+    }
+});
+
+myLayout.registerComponent( 'testComponent', TestComponent );
+
+myLayout.init();
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          Pouet
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
